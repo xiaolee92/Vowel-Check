96eff488f7c168935b398cd75881dccc30ad461e
xiaolee92 <xiaolee1992@yahoo.com>
2021-03-03 20:37:31 +0800

    Repeat function

--- Vowel Checker/Vowel Checker.cpp
+++ Vowel Checker/Vowel Checker.cpp
@@ -4,31 +4,41 @@
 
 int main()
 {
-	std::string inputText;
-	const char vowels[5] = {'a','e','i','o','u'};
-	int indVowels[] = { 0,0,0,0,0 };
-	std::string words;
+	const char vowels[5] = { 'a','e','i','o','u' };
+	
+	std::string again = "y";
 
-	std::cout << "Input text:\n";
-	std::getline(std::cin, inputText);
+	do {
 
-	int vowelCount= 0;
-	for (int i = 0; i < inputText.size(); i++) {
-		for (int x = 0; x < sizeof(vowels)/sizeof(vowels[0]); x++) {
-			if (tolower(inputText[i]) == vowels[x]) {
-				vowelCount++;
-				indVowels[x]++;
-				
+		std::string inputText;
+		int indVowels[] = { 0,0,0,0,0 };
+		
+		std::cout << "Input text:\n";
+		std::getline(std::cin, inputText);
+
+		int vowelCount = 0;
+		for (int i = 0; i < inputText.size(); i++) {
+			for (int x = 0; x < sizeof(vowels) / sizeof(vowels[0]); x++) {
+				if (tolower(inputText[i]) == vowels[x]) {
+					vowelCount++;
+					indVowels[x]++;
+				}
 			}
 		}
-	}
-	
-	for (int i = 0; i < sizeof(indVowels) / sizeof(indVowels[0]); i++) {
-		if (indVowels[i] > 0) {
-			std::cout << "Vowel " << vowels[i] << ": " << indVowels[i] << "\n";
+		for (int i = 0; i < sizeof(indVowels) / sizeof(indVowels[0]); i++) {
+			if (indVowels[i] > 0) {
+				std::cout << "Vowel " << vowels[i] << ": " << indVowels[i] << "\n";
+			}
 		}
-	}
+		std::cout << "Total vowels: " << vowelCount << "\n\n";
+		std::cout << "Would you like to check another word? (y/n): ";
+		std::getline(std::cin, again);
+		system("CLS");
+		
+	} while (again == "y");
 	
-	std::cout << "Total vowels: " << vowelCount << "\n";
+
+	return 0;
+	
 }
 
